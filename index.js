dddb94c55d876ddf67ad88491889e840a67d7b15
joyce wamocho <joywamo@gmail.com>
2024-10-14 13:17:11 +0300

    algorithm

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+    let reverse = word.split('').reverse().join('')
+    return word === reverse; 
 }
+
+
 
 /* 
   Add your pseudocode here
+  reversed word
+  if (the reversed word is equal to word
+  return true
+  else return false
 */
 
 /*
   Add written explanation of your solution here
+  create a function called ispalindrone that recives an argument.the function will check if the argument which 
+  should be a string is the some word when it is reversed and returns true else it will return false
 */
 
 // You can run `node index.js` to view these console logs
